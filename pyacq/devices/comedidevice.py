378a9e7e6ff0e5acab5886e6f629708e312b0f8e
Samuel Garcia <sam/garcia.die@gmail.com>
2015-09-07 14:36:23 +0200

    Some clean but still not tested

--- pyacq/devices/comedidevice.py
+++ pyacq/devices/comedidevice.py
@@ -6,6 +6,8 @@ from ..core import Node, register_node_type
 from pyqtgraph.Qt import QtCore, QtGui
 from pyqtgraph.util.mutex import Mutex
 
+import pycomedi
+
 try:
     import pycomedi
     from pycomedi.device import Device
@@ -20,11 +22,15 @@ try:
 except ImportError:
     HAVE_PYCOMEDI = False
 
+# FIXME : comedi have its own system of AD convertion with a calibration system.
+# question : use it or not ?
+
+#TODO digital device
+
+
 
 #TODO gain/offest by channel or global
-#TODO work on blobal params an by_channel_params
-#TODO channel_indexes, self.ai_channel_ranges,
-#TODO digital device
+
 
 class ComediDevice(Node):
     """
@@ -38,25 +44,28 @@ class ComediDevice(Node):
         Node.__init__(self, **kargs)
         assert HAVE_PYCOMEDI, "ComediDevice node depends on the `pycomedi` package, but it could not be imported."
 
-    def _configure(self, device_path = '/dev/comedi0', sampling_rate = 1000.):
+    def _configure(self, device_path = '/dev/comedi0', sampling_rate = 1000., subdevices_params = None):
         self.device_path = device_path
         self.sampling_rate= sampling_rate
+        if subdevices_params is None:
+            # scan_device_info give the default params
+            subdevices_params = self.scan_device_info(device_path)
+        self.subdevices_params = subdevices_params
         
         # open/prepare/close the device to get the real sampling_rate!!!
         print('sampling_rate wanted:', self.sampling_rate)
         dev = Device(self.device_path)
         dev.open()
-        #TODO channel_indexes, self.ai_channel_ranges,
-        self.ai_subdevice,  ai_channels, internal_size = prepare_device(dev, channel_indexes, self.ai_channel_ranges,  self.sampling_rate)
+        self.prepare_device(dev)
         dev.close()
         print('real_sampling_rate:', self.real_sampling_rate)
 
 
         self.outputs['signals'].spec['shape'] = (-1, self.nb_ai_channel)
-        self.outputs['signals'].spec['dtype = '] = self.dtype
+        self.outputs['signals'].spec['dtype = '] = self.ai_dtype
         self.outputs['signals'].spec['sampling_rate'] = self.real_sampling_rate
-        self.outputs['signals'].spec['gain'] = 1.#TODO
-        self.outputs['signals'].spec['offset'] = 0.#TODO
+        self.outputs['signals'].spec['gain'] = self.ai_gain
+        self.outputs['signals'].spec['offset'] = self.ai_offset
         
     def check_input_specs(self):
         pass
@@ -67,13 +76,11 @@ class ComediDevice(Node):
     def _initialize(self):
         self.dev = Device(self.device_path)
         self.dev.open()
-        #TODO channel_indexes, self.ai_channel_ranges,
-        self.ai_subdevice,  ai_channels, internal_size = prepare_device(self.dev, channel_indexes, self.ai_channel_ranges,  self.sampling_rate)
+        self.prepare_device(self.dev)
 
         self.head = 0
         self.timer = QtCore.QTimer(singleShot = False, interval = 100)
         self.timer.timeout.connect(self.periodic_poll)
-
 
     def _start(self):
         self.last_index = 0
@@ -89,26 +96,19 @@ class ComediDevice(Node):
     def _close(self):
         self.dev.close()
 
-    def get_info(self, device_path):
-        def create_analog_subdevice_param(n):
-            d = {
-                        'type' : 'AnalogInput',
-                        'nb_channel' : n,
-                        'params' :{  }, 
-                        'by_channel_params' : [ {'channel_index' : i, 'selected' : True, range : [-10., 10.] } for i in range(n)],
-                    }
-            return d
+    def scan_device_info(self, device_path):
         
         info = { }
         dev = Device(device_path)
         dev.open()
         info['board_name'] = dev.get_board_name()
-        info['global_params'] = {'sampling_rate' : 4000., 'device_path': device_path}
+        info['device_params'] = {'sampling_rate' : 4000., 'device_path': device_path}
         info['subdevices'] = [ ]
         for sub in dev.subdevices():
             if sub.get_type() == SUBDEVICE_TYPE.ai:
                 n = sub.get_n_channels()
-                info_sub = create_analog_subdevice_param(n)
+                info_sub = {'type' : 'AnalogInput', 'nb_channel' : n, 'subdevice_params' :{ 'channel_range' : [-10., 10.]  }, 
+                            'by_channel_params' : [ {'channel_index' : i, 'selected' : True, } for i in range(n)] }
                 info['subdevices'].append(info_sub)
             #elif sub.get_type() ==  SUBDEVICE_TYPE.di:
                 #TODO digital device
@@ -116,22 +116,36 @@ class ComediDevice(Node):
         dev.close()
         return info
 
-    def prepare_device(self, dev, ai_channel_indexes, ai_channel_ranges, sampling_rate):
-        self.nb_ai_channel = len(ai_channel_indexes)
-        #~ dev.parse_calibration()
+    def prepare_device(self, dev ):
+        #Note : only AI for the moment so the first of the list
+        ai_params = self.subdevices_params[0]
         
         self.ai_subdevice = dev.find_subdevice_by_type(SUBDEVICE_TYPE.ai, factory=StreamingSubdevice)
+        self.ai_dtype = np.dtype(self.ai_subdevice.get_dtype())
+        
         aref = AREF.common # AREF.diff,   AREF.grounds
         
-        self.ai_channels = [ self.ai_subdevice.channel(int(i), factory=AnalogChannel, aref=aref) for i in ai_channel_indexes]
-        for chan, range_ in zip(self.ai_channels, ai_channel_ranges):
-            chan.range = chan.find_range(unit=UNIT.volt, min=range_[0], max=range_[1])
+        # TODO range : check this with device
+        rmin, rmax = channel_range = ai_params['subdevice_params']['channel_range']
+        phys_range = rmax - rmin
+        logic_range = np.iinfo(dt).max-np.iinfo(dt).min
+        self.ai_gain = phys_range/logic_range
+        if self.ai_dtype.name.startswith('u'):
+            self.ai_offset = rmin
+        else:
+            self.ai_offset = 0.
         
-        self.dtype = np.dtype(self.ai_subdevice.get_dtype())
-        itemsize = self.dtype.itemsize
+        self.ai_channels = []
+        for ai_param in ai_params:
+            if ai_param['selected']:
+                chan = self.ai_subdevice.channel(ai_param['channel_index'], factory=AnalogChannel, aref=aref)
+                chan.range = chan.find_range(unit=UNIT.volt, min=rmin, max=rmax)
+                self.ai_channels.append(chan)
+        self.nb_ai_channel = len(self.ai_channels)
         
         # need to align to mmap page size
-        resource.getpagesize()
+        
+        itemsize = self.ai_dtype.itemsize
         self.internal_size = int(self.ai_subdevice.get_max_buffer_size()//self.nb_ai_channel//itemsize//mmap.PAGESIZE)* mmap.PAGESIZE
         self.ai_subdevice.set_buffer_size(self.internal_size*self.nb_ai_channel*itemsize)
         
@@ -145,7 +159,7 @@ class ComediDevice(Node):
         ai_cmd.stop_arg = 0
         self.ai_subdevice.cmd = ai_cmd
         
-        # test cmd
+        # test cmd 3 times
         for i in range(3):
             rc = self.ai_subdevice.command_test()
             if rc is not None:
@@ -179,4 +193,4 @@ class ComediDevice(Node):
         ai_subdevice.mark_buffer_read(new_bytes)
 
     
-register_node_type(PyAudio)
+register_node_type(ComediDevice)
